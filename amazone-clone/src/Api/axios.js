ffbd94b317f7f036f29197c8204fb64d9876ec86
Hayat Abera <hayushahayuti@gmail.com>
2025-10-25 20:26:32 +0300

    deployed backend on render

--- amazone-clone/src/Api/axios.js
+++ amazone-clone/src/Api/axios.js
@@ -1,8 +1,12 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL:"http://127.0.0.1:5001/e-clone-f2813/us-central1/api",
+    //local instance of firebase functions
+    // baseURL:"http://127.0.0.1:5001/e-clone-f2813/us-central1/api",
+
+    //deployed version of amazon server render.com
+    baseURL:" https://amazon-api-deploy-cda1.onrender.com",
 });
 
 
-export {axiosInstance}
+export {axiosInstance} 
